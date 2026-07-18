84a3eaabc9620603e7071ab019e924b8490ec33c
amanshukla42357-maker <amanshukla42357@gmail.com>
2026-07-18 10:40:25 +0530

    Refactor comments and fix SQL syntax in init_database.sql
    Updated comments for clarity and corrected SQL syntax for dropping the database.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,15 +1,15 @@
-/*
-=============================================================================
-create data bse and schema
-=============================================================================
-script purpose 
-The scripts a new data base "data_analytics_warehouse" after checking its already exists.
-if the data base exists it is dropped and created new 
-additionally the scripts add 3 schema with in the database : bronze , silver ,gold 
 
-warning 
-Running this scripts will drop the entire "data_analytics_warehouse" database if it exists
-All the data in the database will be permanently deleted . proceed with caution and ensure the entire backups of the script
+--=============================================================================
+--create data bse and schema
+--=============================================================================
+--script purpose 
+--The scripts a new data base "data_analytics_warehouse" after checking its already exists.
+--if the data base exists it is dropped and created new 
+--additionally the scripts add 3 schema with in the database : bronze , silver ,gold 
+
+--warning 
+--Running this scripts will drop the entire "data_analytics_warehouse" database if it exists
+--All the data in the database will be permanently deleted . proceed with caution and ensure the entire backups of the script
 
 
 use master 
@@ -17,7 +17,7 @@ go ;
 -- drop and create "data_analytics_warehouse" if exists 
 if exists (select 1 sys.database where name  = "data_analytics_warehouse")
 begin 
-     alter data ware house set single_user with roll backup immidiate ;
+     alter database data_analytics_warehouse set single_user with roll backup immidiate ;
 drop database data_analytics_warehouse
 end 
  -- create database "data_analytics_warehouse" 
